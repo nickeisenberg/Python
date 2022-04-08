69dcaecb7023feed681f0ae6d949e088c2feefaa
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-04-08 10:02:45 -0500

    removed a folder

--- Py4E_exercises/chap11/ex1.py
+++
@@ -1,9 +0,0 @@
-inp = input('Enter a regular expression: ')
-fhand = open('mbox.txt')
-count = 0
-import re
-for line in fhand:
-    line =  line.rstrip()
-    if re.search(inp,line):
-        count = count + 1
-print(count)
--- Py4E_exercises/chap8/ex2.py
+++
@@ -1,6 +0,0 @@
-fhand=open('mbox-short.txt')
-for line in fhand:
-    words = line.split()
-    if len(words) == 0: continue
-    if words[0] != 'From': continue
-    print(words[2])
