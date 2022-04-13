790d45c4cb117fc43f9868a77b0a640fbe7b8552
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-04-13 15:02:33 -0500

    animation for brownian sample path

--- Misc/PlotExamples/twosubplots.py
+++ Misc/PlotExamples/twosubplots.py
@@ -5,12 +5,27 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 2*np.pi, 400)
 y = np.sin(x**2)
 
-# Create two subolots side by side
-f, (ax1, ax2) = plt.subplots(2, sharey=True)
-ax1.plot(x, y)
-ax1.set_title('ax_1 title')
-ax2.scatter(x, y)
-ax2.set_title('ax_2 title')
+## Create two subolots side by side
+#f, (ax1, ax2) = plt.subplots(2, sharey=True)
+#ax1.plot(x, y)
+#ax1.set_title('ax_1 title')
+#ax2.scatter(x, y)
+#ax2.set_title('ax_2 title')
+
+# Another method 
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+ax1.plot(x,y)
+ax2.scatter(x,y)
+
+## Good example illustrationg how to use fig.add_subplot()
+#fig = plt.figure()
+#fig.add_subplot(221)   #top left
+#fig.add_subplot(222)   #top right
+#fig.add_subplot(223)   #bottom left
+#fig.add_subplot(224)   #bottom right
+#plt.show()
 
 ## Create two subplots stacked vertically
 #f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
--- Stochastic_Simulations/Brownian Motion/SamplePathAni.py
+++ Stochastic_Simulations/Brownian Motion/SamplePathAni.py
@@ -2,11 +2,11 @@
 
 import numpy as np
 import matplotlib.pyplot as plt 
-from matplotlib import animation
+from matplotlib.animation import FuncAnimation
 
 # create the time interval and partition 
 t = 2.5 
-n = 200 
+n = 100 
 
 # How many sample paths?
 path_amt = 2
@@ -27,21 +27,48 @@ def sample_paths(i, t ,n):
 
 B_paths = sample_paths(path_amt, t, n)
 
-# Create an animation of the first sample path
+# Create the animation function for the sample path
+x = []
+y = []
 t_axis = np.linspace(0, t, n+1)
 
-t = []
-y = []
+fig, ax = plt.subplots()
 
-for amt in range(path_amt):
-   for i in range(n+1):
-        t.append(t_axis[i])
-        y.append(B_paths[amt][i])
+ax.set_xlim(0, 3)
+ax.set_ylim(-4, 4)
 
-        plt.xlim(0,3)
-        plt.ylim(-3,3)
+line, = ax.plot(0, 0)
 
-        plt.plot(t, y)
-        plt.pause(0.0000001)
+def anim_func(i):
+    x.append(t_axis[int(i * n / t)])
+    y.append(B_paths[0][int(i * n / t)])
+
+    line.set_xdata(x)
+    line.set_ydata(y)
+    return line,
+
+animation = FuncAnimation(fig, func = anim_func, \
+                frames = np.linspace(0, t, n+1), interval = 10)
 
 plt.show()
+
+
+
+# Create an animation of the first sample path
+#t_axis = np.linspace(0, t, n+1)
+#
+#t = []
+#y = []
+#
+#for amt in range(path_amt):
+#   for i in range(n+1):
+#        t.append(t_axis[i])
+#        y.append(B_paths[amt][i])
+#
+#        plt.xlim(0,3)
+#        plt.ylim(-3,3)
+#
+#        plt.plot(t, y)
+#        plt.pause(0.0000001)
+
+#plt.show()
