b028396c486adc15a98ee58a7d3f098944e4bb79
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-04-19 11:19:29 -0500

    added option to chose more paths

--- Stochastic_Simulations/Brownian Motion/brownianmotion.py
+++ Stochastic_Simulations/Brownian Motion/brownianmotion.py
@@ -10,17 +10,21 @@ t=5
 n=1000
 delta_t = t/n
 
-# For i \in P, recall that W_i - W_{i-1} ~ N(0,t/n)
-# We sample 1000 samples from n(0,t/n), {dW_{i-1}}_{1 \le i \le 1000}
-# We approximate W_j by W_j = W_{j-1} + dW_{j-1}
-# Through iteration, W_j = dW_{0} + ... + dW_{j-1}
+# how many paths
+pamt = 2
 
-dW = np.sqrt(delta_t) * np.random.normal(0,1,size =n )
+# create an array to store the paths
+X = np.zeros((pamt, n+1))
 
-W = np.empty(n+1, dtype=float)
-W[0] = 0
-for i in range(n):
-    W[i+1] = W[i] + dW[i]
+# create the paths 
+for i in range(pamt):        
+    dW = np.sqrt(delta_t) * np.random.normal(0,1,size =n )
+    
+    W = np.empty(n+1, dtype=float)
+    W[0] = 0
+    for j in range(n):
+        W[j+1] = W[j] + dW[j]
+    X[i, : ] = W
 
 # There is a bulit in formula, np.cumsum, that does what the above for loop does.
 # W = Wt
@@ -30,12 +34,11 @@ for i in range(n):
 
 
 # x-axis for the plot
+x = np.linspace(0, t, n + 1)
 
-x = np.zeros(1)
-for i in range(n):
-    xi = delta_t * (i+1)
-    x = np.append(x,xi)
+# plot the paths
+for i in range(pamt):
+    plt.plot(x,X[i])
 
-plt.plot(x,W)
 plt.title('Brownian Motion')
 plt.show()
