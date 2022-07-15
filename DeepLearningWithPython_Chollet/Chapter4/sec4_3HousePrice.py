bb7b69edd1df7b1945e45136a90a86e99b4d0190
nickeisenberg <64921400+nickeisenberg@users.noreply.github.com>
2022-07-15 14:20:24 -0700

    chapter 4 examples

--- DeepLearningWithPython_Chollet/Chapter4/sec4_3HousePrice.py
+++ DeepLearningWithPython_Chollet/Chapter4/sec4_3HousePrice.py
@@ -50,4 +50,47 @@ for i in range(k):
     all_scores.append(val_mae)
 
 # Listing 4.27 Saving the validation logs at each fold 
+num_epochs = 500
+all_mae_histories = []
+for i in range(k):
+    print(f'Processing fold #{i}')
+    val_data = train_data[i*num_val_samples : (i+1)*num_val_samples]
+    val_targets = train_targets[i*num_val_samples : (i+1)*num_val_samples]
+    partial_train_data = np.concatenate(
+        [train_data[: i*num_val_samples],
+         train_data[(i+1)*num_val_samples:]],
+        axis=0)
+    partial_train_targets = np.concatenate(
+        [train_targets[: i*num_val_samples],
+         train_targets[(i+1)*num_val_samples:]],
+        axis=0)
+    model = build_model()
+    history = model.fit(partial_train_data,
+              partial_train_targets,
+              epochs=num_epochs,
+              batch_size=16,
+              validation_data=(val_data, val_targets),
+              verbose=0)
+    mae_history = history.history['val_mae']
+    all_mae_histories.append(mae_history)
+
+# Listing 4.28 Building the history of successive mean K-fold validation scores
+average_mae_history = [
+    np.mean([x[i] for x in all_mae_histories]) for i in range(num_epochs)]
+
+# Listing 4.29 Plotting validation scores
+import matplotlib.pyplot as plt
+plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
+# Listing 4.30 PLotting validation scored, excluding the first 10 data points
+truncated_mae_history = average_mae_history[10:]
+plt.clf()
+plt.plot(range(1, len(truncated_mae_history) + 1), truncated_mae_history)
+plt.xlabel('Epochs')
+plt.ylabel('Validation MAE')
+plt.show()
+
 
